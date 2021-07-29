5c6f02d750eb178165fe185f8c84a30756cfd11e
THAMYRES HELOISA ZOMER <thamyres.zomer@alunos.sc.senac.br>
2021-07-29 17:14:52 -0300

    29/07/21

---
+++ Aula 52/aula/modulosEs6/index.js
@@ -0,0 +1,15 @@
+import {soma } from "./utils"
+import cowsay from "cowsay";
+
+console.log(soma(1,2));
+
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+    text : "I'm a moooodule",
+    e : "oO",
+    T : "U "
+}));
+
+// or cowsay.think()
