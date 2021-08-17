8e2ffdda0822f1fc6f641a73c006ae08a56a2d00
THAMYRES HELOISA ZOMER <thamyres.zomer@alunos.sc.senac.br>
2021-08-17 17:05:44 -0300

    15/08/21 atualizado

--- Aula 62/pratica/database/index.js
+++ Aula 62/pratica/database/index.js
@@ -1,20 +1,32 @@
 require("dotenv").config();
-const{ Sequelize } = require("sequelize")
+const { Sequelize } = require("sequelize");
 const dbConfig = require("../config/database");
 
-console.log(dbConfig);
+const Usuario = require("../models/Usuario");
+const Projeto = require("../models/Projeto");
+const Endereco = require("../models/Endereco");
 
 const sequelize = new Sequelize(dbConfig);
 
+// Inicializando os models
+Usuario.init(sequelize);
+Projeto.init(sequelize);
+Endereco.init(sequelize);
+
+// Definindo as associações para os models
+Usuario.associate(sequelize.models);
+Projeto.associate(sequelize.models);
+Endereco.associate(sequelize.models);
+
 module.exports = sequelize;
-//  testando conecção
+// Testando conexão
 // (async () => {
-//     try {
-//         await sequelize.authenticate();
-//         console.log('Conecção bem-sucedida!');
-//     } catch (error) {
-//         console.log('Conexão falhou!', error);
-//     } finally {
-//         sequelize.close();
-//     }
+// try {
+// await sequelize.authenticate();
+// console.log('Conexão bem-sucedida!');
+// } catch (error) {
+// console.error('Conexão falhou!', error);
+// } finally {
+// sequelize.close();
+// }
 // })();
