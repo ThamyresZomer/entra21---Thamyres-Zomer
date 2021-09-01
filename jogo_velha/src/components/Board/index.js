7833e4f2e21db2c942404d4e17b11923c118e0b1
THAMYRES HELOISA ZOMER <thamyres.zomer@alunos.sc.senac.br>
2021-09-01 16:54:49 -0300

    01/09/21 atualizado

--- jogo_velha/src/components/Board/index.js
+++ jogo_velha/src/components/Board/index.js
@@ -1,4 +1,27 @@
-class Board extends React.Component {
+import React from 'react';
+import {Square} from '../Square'
+ 
+function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
+export class Board extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -6,29 +29,31 @@ class Board extends React.Component {
             xIsNext: true,
         };
     }
+    
     handleClick(i) {
         const squares = this.state.squares.slice();
-        squares[i] =  this.state,xIsNext ? 'X' : "0";
-        this.setState({
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+          }
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState ({
             squares: squares,
-         xIsNext: !this.state.xIsNext,
-        });
+            xIsNext: !this.state.xIsNext,
+        }); 
       }
+ 
      renderSquare(i) {
         return (
             <Square
-              value={this.state.squares[i]}
-              onClick={() => this.handleClick(i)}
+              value={this.props.squares[i]}
+              onClick={() => this.props.onClick(i)}
             />
           );
         }
-
+ 
     render() {
-        const status = 'Next player: X' + (this.state.xIsNext ? 'X' : 'O');;
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -48,3 +73,7 @@ class Board extends React.Component {
         );
     }
 }
+
+
+
+  
--- jogo_velha/src/components/Square/index.js
+++ jogo_velha/src/components/Square/index.js
@@ -1,7 +1,11 @@
-function Square(props) {
+import React from 'react';
+
+export function Square(props) {
     return (
       <button className="square" onClick={props.onClick}>
         {props.value}
       </button>
     );
   }
+
+ 
